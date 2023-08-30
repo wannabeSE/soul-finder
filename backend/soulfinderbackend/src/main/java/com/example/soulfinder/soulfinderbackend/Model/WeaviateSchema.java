38967f3f1f33c3627613b4c948a6b3b243908aae
wannabeSE <samirk724@gmail.com>
2023-08-30 15:02:24 +0600

    image seach is active and running

--- backend/soulfinderbackend/src/main/java/com/example/soulfinder/soulfinderbackend/Model/WeaviateSchema.java
+++ backend/soulfinderbackend/src/main/java/com/example/soulfinder/soulfinderbackend/Model/WeaviateSchema.java
@@ -1,45 +1,53 @@
 package com.example.soulfinder.soulfinderbackend.Model;
 
-// import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-// import io.weaviate.client.Config;
-// import io.weaviate.client.WeaviateClient;
-// import io.weaviate.client.base.Result;
-// import io.weaviate.client.v1.schema.model.WeaviateClass;
+import io.weaviate.client.Config;
+import io.weaviate.client.WeaviateClient;
+
+
+import io.weaviate.client.base.Result;
+import io.weaviate.client.v1.schema.model.WeaviateClass;
 
 public class WeaviateSchema {
-    // public static void main(String[] args) {
-    //     Config config = new Config("http", "localhost:8080");
-    //     WeaviateClient client = new WeaviateClient(config);
-    //     Map<String, Object> img2vec = new HashMap<>();
-    //     HashMap <String, Object> img2vecNeural = new HashMap<>();
-    //     ArrayList <String> imageFields = new ArrayList<>();
-    //     imageFields.add("image");
-    //     img2vecNeural.put("imageFields", imageFields);
-    //     img2vec.put("img2vec-neural", img2vecNeural);
+
+    public static WeaviateClient retConfig(){
+        Config config = new Config("http","localhost:8080");
+        WeaviateClient client = new WeaviateClient(config);
+        return client;
+    }
+    public void schemaDesign(){
+        Map<String, Object> img2vec = new HashMap<>();
+        HashMap <String, Object> img2vecNeural = new HashMap<>();
+        ArrayList <String> imageFields = new ArrayList<>();
+        imageFields.add("image");
+        img2vecNeural.put("imageFields", imageFields);
+        img2vec.put("img2vec-neural", img2vecNeural);
+        WeaviateClass clazz = WeaviateClass.builder()
+        .className("Test")
+        .vectorizer("img2vec-neural")
+        .vectorIndexType("hnsw")
+        .moduleConfig(img2vec)
+        .properties(new ArrayList<>(){{
+            add(io.weaviate.client.v1.schema.model.Property.builder()
+            .dataType(new ArrayList<>(){{
+                add("blob");
+            }})
+            .name("image")
+            .build());
+        }})
+        .build();
         
-    //     WeaviateClass clazz = WeaviateClass.builder()
-    //     .className("Test")
-    //     .vectorizer("img2vec-neural")
-    //     .vectorIndexType("hnsw")
-    //     .moduleConfig(img2vec)
-    //     .properties(new ArrayList<>(){{
-    //         add(io.weaviate.client.v1.schema.model.Property.builder()
-    //         .dataType(new ArrayList<>(){{
-    //             add("blob");
-    //         }})
-    //         .name("image")
-    //         .build());
-    //     }})
-    //     .build();
-        
-    //     Result <Boolean> dbResult = client.schema().classCreator().withClass(clazz).run();
-    //     if(dbResult.hasErrors()){
-    //         System.out.println("error occured");
-    //         return;
-    //     }
-    //     System.out.println("db created successfully");
-    // }
+        Config config = new Config("http","localhost:8080");
+        WeaviateClient client = new WeaviateClient(config);
+
+        Result <Boolean> dbResult = client.schema().classCreator().withClass(clazz).run();
+        if(dbResult.hasErrors()){
+            System.out.println("error occured");
+            return;
+        }
+        System.out.println("db created successfully");
+    }
 }
---
+++ backend/soulfinderbackend/src/main/java/com/example/soulfinder/soulfinderbackend/Service/VectorDBService.java
@@ -0,0 +1,109 @@
+package com.example.soulfinder.soulfinderbackend.Service;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.soulfinder.soulfinderbackend.Model.WeaviateSchema;
+
+import io.weaviate.client.WeaviateClient;
+import io.weaviate.client.base.Result;
+import io.weaviate.client.v1.data.model.WeaviateObject;
+import io.weaviate.client.v1.graphql.model.GraphQLResponse;
+import io.weaviate.client.v1.graphql.query.argument.NearImageArgument;
+import io.weaviate.client.v1.graphql.query.fields.Field;
+
+@Service
+public class VectorDBService {
+    
+ 
+    //WeaviateClient client = WeaviateSchema.retConfig();
+    public static String fileEncoder(MultipartFile fl) throws IOException{
+        
+        byte [] fileContent = fl.getBytes();
+
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+        return encodedString;
+    }
+    
+    public static String vectorDbImageUploader(MultipartFile fl) throws IOException{
+        WeaviateClient client = WeaviateSchema.retConfig();
+        Map<String, Object> dataSchema = new HashMap<>();
+        String encodedString = fileEncoder(fl);
+        dataSchema.put("image", encodedString);
+        Result<WeaviateObject> result = client.data().creator()
+        .withClassName("Test")
+        .withProperties(dataSchema)
+        .run();
+
+        if(result.hasErrors()){
+            System.out.println("file upload failed");
+            return "failed";
+        }System.out.println("Successfully uploaded");
+        return "Ok";
+    }
+
+    public static Object dbItemCounter(){
+        WeaviateClient client = WeaviateSchema.retConfig();
+        Field meta = Field.builder()
+            .name("meta")
+            .fields(new Field[]{
+                Field.builder().name("count").build()
+            }).build();
+        Result<GraphQLResponse> result = client.graphQL().aggregate()
+            .withClassName("Test")
+            .withFields(meta)
+            .run();
+        if(result.hasErrors()){
+            return result.getError();
+        }
+        return result.getResult();
+    }
+
+    public static void imageSearchVectorDB(MultipartFile file){
+        WeaviateClient client = WeaviateSchema.retConfig();
+        String encodedString = "";
+        try {
+            encodedString = fileEncoder(file);
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+        
+        NearImageArgument test = client.graphQL().arguments().nearImageArgBuilder()
+        .image(encodedString)
+        .build();
+        Field image = Field.builder().name("image").build();
+        Result<GraphQLResponse> result = client.graphQL().get()
+            .withClassName("Test")
+            .withFields(image)
+            .withNearImage(test)
+            .withLimit(2)
+            .run();
+        if (result.hasErrors()) {
+            System.out.println(result.getError());
+            return;
+        }
+        var responseImage = ((Map)((ArrayList)((Map)((Map)result.getResult().getData()).get("Get")).get("Test")).get(0)).get("image");
+        var responseImage2 = ((Map)((ArrayList)((Map)((Map)result.getResult().getData()).get("Get")).get("Test")).get(1)).get("image");
+        try {
+            imageConverter(responseImage.toString(), "1");
+            imageConverter(responseImage2.toString(), "2");
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+    public static void imageConverter(String encodedString, String img) throws IOException{
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        FileUtils.writeByteArrayToFile(new File(img+".jpg"), decodedBytes);
+    }
+}

d6f370290bce6199678e87a4af5c909f1cea45fd
wannabeSE <samirk724@gmail.com>
2023-09-10 19:06:11 +0600

    mongodb connected and running

---
+++ backend/soulfinderbackend/src/main/java/com/example/soulfinder/soulfinderbackend/Controller/PostController.java
@@ -0,0 +1,36 @@
+package com.example.soulfinder.soulfinderbackend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.soulfinder.soulfinderbackend.Model.Post;
+import com.example.soulfinder.soulfinderbackend.Service.PostService;
+
+@RestController
+@RequestMapping("api/post/")
+public class PostController {
+    
+    @Autowired
+    PostService postService;
+
+    @GetMapping("/")
+    public String hello(){
+        return "server is up";
+    }
+
+    @GetMapping("/get-posts")
+    public Object getPost(){
+        Object posts = postService.getAllPostService();
+        return posts;
+    }
+
+    @PostMapping("/save-post")
+    public Post postToDb(@RequestBody Post post){
+        postService.savePostService(post);
+        return post;
+    }
+}
